fe4a03566462f90d86fd57e2f508fcd8733ced9d
Erik Jan de Wit <erikjan.dewit@gmail.com>
2019-11-21 13:10:53 +0100

    initial version of script

---
+++ README.md
@@ -0,0 +1,11 @@
+=== Little script to delete all users
+
+Here you go George, a script that deletes all users
+change the url to connect to and the username / password
+
+to build and run:
+
+```
+$> npm install
+$> npm start
+```
---
+++ index.ts
@@ -0,0 +1,53 @@
+import { readdirSync, PathLike } from 'fs';
+import fetch from 'node-fetch';
+
+const getDirectories = (source: PathLike) =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
+
+const Octokit = require("@octokit/rest");
+
+const octokit = Octokit({
+  auth: '<token>',
+  userAgent: 'quarkus contributers search'
+});
+
+(async () => {
+  const base = './extensions';
+  const userNames: any = [];
+  getDirectories(base).forEach(dir => {
+    const simpleGit = require('simple-git')(base + '/' + dir);
+    userNames.push(new Promise<string>((resolve, reject) => {
+      simpleGit.log({ '--reverse': null, file: 'pom.xml' },
+        (err: any, log: any) => {
+          if (!err) {
+            resolve(log.latest.author_name);
+          } else {
+            resolve('');
+          }
+        });
+    }));
+  });
+
+  const unique = new Set(await Promise.all(userNames));
+  const users = Array.from(unique.values());
+
+  users.forEach(async (user) => {
+    const result = await octokit.search.users({
+      q: 'fullname:' + user
+    });
+
+    if (result.data.items[0]) {
+      const response = await fetch(result.data.items[0].html_url);
+      if (response.ok) {
+        const isRedhat = (await response.text()).indexOf('Red Hat') != -1;
+        console.log(user + ' -> ' + (isRedhat ? 'from Red Hat' : 'external'));
+      } else {
+        console.error("HTTP-Error: " + response.status);
+      }
+    } else {
+      console.log('user not found', user);
+    }
+  });
+})();
